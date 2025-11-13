a2e44e39b99cb7082debb8d615cf9bedc18bc463
Mudassar <mudassar.h1200@gmail.com>
2025-11-13 15:13:40 +0500

    Updated the getStorage functions so that it handle both s3 and r2 client request from the client side

--- cloud/storage/index.ts
+++ cloud/storage/index.ts
@@ -1,14 +1,34 @@
-import { createR2Client } from "../client";
+import { createR2Client, createS3Client } from "../client";
 import { R2Storage } from "./r2-operations";
+import { S3Storage } from "./s3-operations";
 import { Storage } from "./storage"
 
-let storageInstance: Storage | null = null;
+type Mode = "r2" | "s3"
+const storageCache: Partial<Record<Mode, Storage>> = {}
 
-export function getStorage(): Storage {
-    if (storageInstance) return storageInstance;
+export function getStorage(mode: string | null): Storage {
+    console.log("Mode in the getStorage is: ", mode)
+    const normalized = (mode || '') as Mode;
+
+    if (normalized !== "r2" && normalized !== "s3") {
+        throw new Error(`Invalid storage mode: ${mode}`);
+    }
+
+    if (storageCache[normalized]) return storageCache[normalized];
+
+    if (normalized === 's3') {
+        const bucket = process.env.S3_BUCKET_NAME;
+        if (!bucket) throw new Error("Bucket not found or invalid");
+        const client = createS3Client();
+        const instance = new S3Storage(client, bucket);
+        storageCache.s3 = instance
+        return instance;
+    }
+
     const bucket = process.env.R2_BUCKET_NAME;
     if (!bucket) throw new Error("Bucket not found or invalid");
     const client = createR2Client();
-    storageInstance = new R2Storage(client, bucket);
-    return storageInstance;
+    const instance = new R2Storage(client, bucket);
+    storageCache.r2 = instance
+    return instance;
 }
